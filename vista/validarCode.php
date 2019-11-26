2f7c14955f795c9d322f5a89353730e21ddb905b
AlfonsoCortez <47646157+AlfonsoCortez@users.noreply.github.com>
2019-11-26 06:04:38 -0500

    validar

--- vista/validarCode.php
+++ vista/validarCode.php
@@ -5,18 +5,16 @@ include '../helps/helps.php';
 
 session_start();
 
-header('Content-type: application/json');
-$resultado = array();
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["txtUsuario"]) && isset($_POST["txtPassword"])) {
+    if (isset($_POST["txtNombre"]) && isset($_POST["txtEmail"]) && isset($_POST["txtUsuario"]) && isset($_POST["txtPassword"])) {
 
-        $txtUsuario  = validar_campo($_POST["txtUsuario"]);
-        $txtPassword = validar_campo($_POST["txtPassword"]);
+        $txtNombre     = validar_campo($_POST["txtNombre"]);
+        $txtEmail      = validar_campo($_POST["txtEmail"]);
+        $txtUsuario    = validar_campo($_POST["txtUsuario"]);
+        $txtPassword   = validar_campo($_POST["txtPassword"]);
+        $txtPrivilegio = 2;
 
-        $resultado = array("estado" => "true");
-
-        if (UsuarioControlador::login($txtUsuario, $txtPassword)) {
+        if (UsuarioControlador::registrar($txtNombre, $txtEmail, $txtUsuario, $txtPassword, $txtPrivilegio)) {
             $usuario             = UsuarioControlador::getUsuario($txtUsuario, $txtPassword);
             $_SESSION["usuario"] = array(
                 "id"         => $usuario->getId(),
@@ -25,11 +23,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 "email"      => $usuario->getEmail(),
                 "privilegio" => $usuario->getPrivilegio(),
             );
-            return print(json_encode($resultado));
+
+            header("location:admin.php");
         }
 
     }
+} else {
+    header("location:registro.php?error=1");
 }
-
-$resultado = array("estado" => "false");
-return print(json_encode($resultado));
