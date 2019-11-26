656a27506ba1cf76a9094ce704900f481d51ce6a
AlfonsoCortez <47646157+AlfonsoCortez@users.noreply.github.com>
2019-11-26 07:11:02 -0500

    cambio6

--- vista/registroCode.php
+++ vista/registroCode.php
@@ -5,16 +5,16 @@ include '../helps/helps.php';
 
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["txtNombre"]) && isset($_POST["txtEmail"]) && isset($_POST["txtUsuario"]) && isset($_POST["txtPassword"])) {
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // if (isset($_POST["txtNombre"])  && isset($_POST["txtUsuario"]) && isset($_POST["txtEmail"]) && isset($_POST["txtPassword"])) {
 
         $txtNombre     = validar_campo($_POST["txtNombre"]);
-        $txtEmail      = validar_campo($_POST["txtEmail"]);
         $txtUsuario    = validar_campo($_POST["txtUsuario"]);
+        $txtEmail      = validar_campo($_POST["txtEmail"]);
         $txtPassword   = validar_campo($_POST["txtPassword"]);
         $txtPrivilegio = 2;
 
-        if (UsuarioControlador::registrar($txtNombre, $txtEmail, $txtUsuario, $txtPassword, $txtPrivilegio)) {
+        if (UsuarioControlador::registrar($txtNombre,  $txtUsuario, $txtEmail, $txtPassword, $txtPrivilegio)) {
             $usuario             = UsuarioControlador::getUsuario($txtUsuario, $txtPassword);
             $_SESSION["usuario"] = array(
                 "id"         => $usuario->getId(),
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             header("location:admin.php");
         }
 
-    }
-} else {
-    header("location:registro.php?error=1");
-}
+    // }
+// } else {
+//     header("location:registro.php?error=1");
+// }
