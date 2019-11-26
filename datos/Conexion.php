afeae61177b281a062e1628593b2636620bba87d
AlfonsoCortez <47646157+AlfonsoCortez@users.noreply.github.com>
2019-11-26 05:21:02 -0500

    correcion 5

--- datos/Conexion.php
+++ datos/Conexion.php
@@ -11,8 +11,8 @@ class Conexion
     public static function conectar()
     {
         try {
-            // $cn = new PDO("mysql:host=127.0.0.1:52225;dbname=proyectotipetdb;charset=utf8mb4", "azure", "6#vWHD_$");
-            $cn = new PDO("Data Source=tcp:servidorproyectotipet.database.windows.net,1433;Initial Catalog=ProyectoTipet;User Id=AlfonsoChavarro@servidorproyectotipet.database.windows.net;Password=JoseCortez3166226046");
+            $cn = new PDO("mysql:host=127.0.0.1:52225;dbname=proyectotipetdb;charset=utf8mb4", "azure", "6#vWHD_$");
+            // $cn = new PDO("Data Source=tcp:servidorproyectotipet.database.windows.net,1433;Initial Catalog=ProyectoTipet;User Id=AlfonsoChavarro@servidorproyectotipet.database.windows.net;Password=JoseCortez3166226046");
 
             return $cn;
 
